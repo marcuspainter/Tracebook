acb93946caf573df63056b1b554c39ebf4d49430
Marcus Painter <marcus@marcuspainter.com>
2023-12-07 00:43:24 +0000

    Initial Commit

---
+++ Tracebook/TracebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  TracebookApp.swift
+//  Tracebook
+//
+//  Created by Marcus Painter on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TracebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
