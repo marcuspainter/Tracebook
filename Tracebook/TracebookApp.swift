9c66281fa2bc8a828e07fc8aae3907e518212da9
Marcus Painter <marcus@marcuspainter.com>
2024-11-05 03:23:27 +0000

    Add Firebase Analytics

--- Tracebook/TracebookApp.swift
+++ Tracebook/TracebookApp.swift
@@ -6,10 +6,20 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct TracebookApp: App {
     @Environment(\.scenePhase) var scenePhase
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
