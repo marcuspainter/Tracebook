a17f4949d0256519fcf7e967c48fdb21231de1b9
Marcus Painter <marcus@marcuspainter.com>
2024-11-07 19:56:53 +0000

    Separate AppDelegate file

---
+++ Tracebook/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Tracebook
+//
+//  Created by Marcus Painter on 05/11/2024.
+//
+
+import UIKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
